fb1e6281ea906bb3ed86456badc889336ec2cde1
Satoshi <satoshi.taenaka@gmail.com>
2023-11-27 19:37:00 +0900

    Re Take 7

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            '/home/mysite/blog/templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
